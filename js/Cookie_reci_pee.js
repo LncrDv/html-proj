0fef5e9afc4a588809c91d928e9397ca640681a0
AXOL0RD <serise.timothey@hotmail.com>
2026-02-19 09:43:12 +0100

    More cookies

--- js/Cookie_reci_pee.js
+++ js/Cookie_reci_pee.js
@@ -1,7 +1,21 @@
-const button = document.getElementById("myButton");
-const counterDisplay = document.getElementById("counter");
+const button = document.querySelector("#cookieCounter");
+const counterDisplay = document.querySelector("#counter");
 
+var value = 1
 button.addEventListener("click", () => {
     value += 1;
-    counterDisplay.textContent = value;
+    UpdateDisplay();
     });
+
+function UpdateDisplay() {
+    counterDisplay.innerText = value;
+    document.querySelector("#ingredient1").textContent = 30*value;
+    document.querySelector("#ingredient2").textContent = 1*value;
+    document.querySelector("#ingredient3").textContent = 1*value;
+    document.querySelector("#ingredient4").textContent = 5*value;
+    document.querySelector("#ingredient5").textContent = 15*value;
+    document.querySelector("#ingredient6").textContent = 15*value;
+    document.querySelector("#ingredient7").textContent = 1*value;
+    document.querySelector("#ingredient8").textContent = 1*value;
+    document.querySelector("#ingredient9").textContent = 20*value;
+}
