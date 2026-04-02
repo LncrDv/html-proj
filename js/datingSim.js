7f157b3f188597231b286f19809ade206b5b63ea
LncrDv <sachunikuothers@gmail.com>
2026-04-02 09:55:28 +0200

    goob goob litterature club

---
+++ js/datingSim.js
@@ -0,0 +1,13 @@
+var gooberTitle_image = document.querySelector("#gooberTitle");
+if (gooberTitle_image) {
+    gooberTitle_image.addEventListener("click", function() {
+        window.location.replace("ds_intro.html");
+    });
+}
+
+var gooberIntro_image = document.querySelector("#gooberIntro");
+if (gooberIntro_image) {
+    gooberIntro_image.addEventListener("click", function() {
+        window.location.replace("ds_title.html");
+    });
+}
